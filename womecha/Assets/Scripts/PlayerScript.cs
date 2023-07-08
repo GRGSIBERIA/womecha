1d2c97c20a115e2f2b087ba447c84ba2dc29f497
Eiichi Takebuchi <nanashi4129@gmail.com>
2023-07-08 13:25:48 +0900

    プレイヤーの基本挙動を追加

--- womecha/Assets/Scripts/PlayerScript.cs
+++ womecha/Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    Rigidbody2D rigid;
+
+    [SerializeField]
+    public float movePower = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void MoveProcedure()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigid.AddForce(Vector2.up * movePower);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigid.AddForce(Vector2.left * movePower);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigid.AddForce(Vector2.down * movePower);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigid.AddForce(Vector2.right * movePower);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveProcedure();
     }
 }
