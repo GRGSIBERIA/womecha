7487eb6befb2753770d42a89caf5a158147864f2
Eiichi Takebuchi <nanashi4129@gmail.com>
2023-07-08 14:59:08 +0900

    回転のキャンセルとジャンプを追加

--- womecha/Assets/Scripts/PlayerScript.cs
+++ womecha/Assets/Scripts/PlayerScript.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     Rigidbody2D rigid;
+    Transform ts;
 
     [SerializeField]
-    public float movePower = 4f;
+    float movePower = 4f;
+
+    [SerializeField]
+    float velocityLimit = 4f;
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        ts = transform;
     }
 
     void MoveProcedure()
@@ -33,6 +38,22 @@ public class PlayerScript : MonoBehaviour
         {
             rigid.AddForce(Vector2.right * movePower);
         }
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            rigid.AddForce(Vector2.up * -Physics2D.gravity * movePower * 0.2f);
+        }
+
+        ts.rotation = Quaternion.identity;  // 回転をキャンセル
+        rigid.angularVelocity = 0f;         // 回転の勢いをキャンセル
+        rigid.totalTorque = 0f;             // 回転の力をキャンセル
+
+        /*
+        // 想定よりも大きい速度が出たらキャンセル
+        if (rigid.velocity.sqrMagnitude > velocityLimit * velocityLimit) 
+        {
+            rigid.velocity = rigid.velocity.normalized * velocityLimit;
+        }
+        */
     }
 
     // Update is called once per frame
